b6acfc2ea0663f91a72e7e016873c6ab3bc12d75
aabboud1 <abboudahmed1@gmail.com>
2020-08-26 19:15:51 -0400

    one more step

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,15 +1,19 @@
 require 'pry'
 class CashRegister
-    attr_accessor :total, :price, :discount
+    attr_accessor :total, :discount, :items
     
     def initialize(discount = 0)
         @total = 0
         @discount = discount
+        @items = []
     end
 
 
     def add_item(title, price, quantity = 1)
-        self.total += price * quantity
+        @total += price * quantity
+        quantity.times do
+            @items << title
+        end
     end
 
     def apply_discount
@@ -22,16 +26,8 @@ class CashRegister
         end
     end
 
-    def items
-        array = []
-        add_item.each do |item|
-            array << item.title
-        end
+    def void_last_transaction
+        
     end
-
-
-
-    
-
 
 end
